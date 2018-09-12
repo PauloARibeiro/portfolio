10df852961cf75e84a81844ba08140d2aa3cfbef
Paulo Ribeiro <eggnator1@gmail.com>
2018-09-12 15:23:03 +0100

    no message

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+
+const apps = express();
+
+app.use("/", serveStatic (path.join(__dirname, '/dist')))
+
+app.get("/", function (req, res)){
+  res.sendFile(__dirname + '/dist/index.html')
+}
+
+const port = process.env.PORT || 5000
+
+app.listen(port);
+
+console.log('Server started port ' + port);
